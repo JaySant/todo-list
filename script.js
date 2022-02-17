50e8304fe96c058e5a89d5e31671ed6946f9b675
Jay Santos <jaysant15@gmail.com>
2022-02-17 18:02:08 -0300

    Requisito 11 e manutençao da indentaçao

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
-function guardaTarefa(){
-  let valorInput = document.getElementById('texto-tarefa').value // valor do input armazenado numa variavel
-  let lis = document.createElement('li') // cria elemento li
-  let textInput = document.createTextNode(valorInput)
+function guardaTarefa() {
+  const valorInput = document.getElementById('texto-tarefa').value; // valor do input armazenado numa variavel
+  const lis = document.createElement('li'); // cria elemento li
+  const textInput = document.createTextNode(valorInput);
 
-  lis.appendChild(textInput) // cria um nó de texto para elemento li criado com o valor do input
-      document.querySelector('ol').appendChild(lis)
-      document.getElementById('texto-tarefa').value=''; //apaga o valor do input
-
+  lis.appendChild(textInput); // cria um nó de texto para elemento li criado com o valor do input
+  document.querySelector('ol').appendChild(lis);
+  document.getElementById('texto-tarefa').value = '';
 }
+guardaTarefa();
 
 // Requisito 7 e 8
-let lista = document.getElementById('lista-tarefas')
-lista.addEventListener('click', event =>{
-  let li = event.target
-  for (let i = 0; i < lista.children.length; i+= 1){
-        lista.children[i].id = ''
-        li.id = 'color'
-      }
-})
+const lista = document.getElementById('lista-tarefas');
+lista.addEventListener('click', (event) => {
+  const li = event.target;
+  for (let i = 0; i < lista.children.length; i += 1) {
+    lista.children[i].id = '';
+    li.id = 'color';
+  }
+});
 
 // Requisito 9
-lista.addEventListener('dblclick', event => {
-  if(event.target.tagName === 'LI'){
-       event.target.classList.toggle('completed')
-   }
-})
+lista.addEventListener('dblclick', (event) => {
+  if (event.target.tagName === 'LI') {
+    event.target.classList.toggle('completed');
+  }
+});
 
-// 
-function apagar(){
-let listaItemAdd = document.getElementById('lista-tarefas')
-listaItemAdd.innerHTML = ""
+function apagar() {
+  const listaItemAdd = document.getElementById('lista-tarefas');
+  listaItemAdd.innerHTML = '';
 }
+apagar();
 
-// apaga o que foi finalizado
-function apagarConcluida(){
- listaItemComplet = document.querySelectorAll('.completed')
- for(i in listaItemComplet){
-      listaItemComplet[i].remove
-     }
+function clean() {
+  const listaComplet = document.querySelectorAll('.completed');
+  for (let index = 0; index < listaComplet.length; index += 1) {
+    listaComplet[index].remove();
+  }
 }
-let btnFinalizado = document.getElementById('remover-finalizados')
-btnFinalizado.addEventListener('click', apagarConcluida)
-  
-// salva as informações de usuario
+clean();
